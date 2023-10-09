99ae12d70fd60f2eaf06eef2c9e4710e0ae93c40
tanishaislam <dev.eva.2006@gmail.com>
2023-10-09 19:40:44 +0600

    last commit

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,6 +4,17 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// console.log('inside fairevsei', import.meta.env.VITE_APIKEY)
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_APIKEY,
+//   authDomain: import.meta.env.VITE_AUTHDOMAIN,
+//   projectId: import.meta.env.VITE_PROJECTID,
+//   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+//   appId: import.meta.env.VITE_APPID,
+// };
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyBBsQfl_K2WKABNVsDaXaqWDzuArluvpgU",
   authDomain: "assaignment-9-a805d.firebaseapp.com",
